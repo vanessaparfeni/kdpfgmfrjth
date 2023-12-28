eec6580ed83a6e89343d120c1f64a6fe42b60579
Sergey Shabanov <shsv382@gmail.com>
2023-12-28 21:16:52 +0300

    Рабочий поиск фильмов

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import Movies from './components/Movies';
+import { useState, useEffect } from 'react';
+import { APIKEY } from './env.js';
+
+import './App.scss';
 
 function App() {
-  const url = "https://reactjs.org"
+  const [movies, setMovies] = useState([]);
+  const [searchField, setSearchField] = useState('');
+  const URL = `https://www.omdbapi.com/?s=${searchField}&apikey=${APIKEY}`;
+
+  function getMovies() {
+    fetch(encodeURI(URL))
+      .then(res => res.json())
+      .then(movies => setMovies(movies.Search))
+  }
+
+  useEffect(getMovies, []);
+  useEffect(getMovies, [searchField]);
+  
+  let timer;
+  function handleChange(event) {
+    clearTimeout(timer);
+
+    timer = setTimeout(() => {
+      setSearchField(event.target.value);
+    }, 1000)
+  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href={url}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" onChange={handleChange} />
+      <Movies movies={movies} />
     </div>
   );
 }
